054aedecc42a767d1303ac8007a07d714fae3ede
Anton Yip <anton_yip@hotmail.com>
2022-01-27 05:58:55 +0800

    feat: Chainwalker blocks+txs and blocks

--- models/core/blocks.sql
+++ models/core/blocks.sql
@@ -2,30 +2,59 @@
     config(
         materialized='incremental',
         unique_key='block_id',
+        incremental_strategy = 'delete+insert',
         tags=['core'],
         cluster_by=['block_timestamp']
     )
 }}
 
--- WIP -- below serves as example
-with base_blocks as (
+with
 
-    select * from {{ deduped_blocks("near_blocks") }}
+base_blocks as (
+
+    select
+        *
+    from {{ ref("chainwalker_blocks") }}
+    where {{ incremental_load_filter("block_timestamp") }}
 
 ),
 final as (
-    select
-        block_id,
-        block_timestamp,
-        header:hash::string as block_hash,
-        header:parent_hash::string as block_parent_hash,
-        header:miner::string as miner,
-        header:size as size,
-        -- Build out more columns here from `header`
-        -- ...
 
+    select
+        header:height::integer as block_id,
+        header:timestamp::timestamp as block_timestamp,
+        header:hash::string as block_hash,
+        header:tx_count::integer as tx_count,
+        header:author::string as block_author,
+        header:challenges_result as block_challenges_result,
+        header:challenges_root::string as block_challenges_root,
+        header:chunk_headers_root::string as chunk_headers_root,
+        header:chunk_mask as chunk_mask,
+        header:chunk_receipts_root::string as chunk_receipts_root,
+        header:chunk_tx_root::string as chunk_tx_root,
+        header:chunks::string as chunks,
+        header:chunks_included::string as chunks_included,
+        header:epoch_id::string as epoch_id,
+        header:epoch_sync_data_hash::string as epoch_sync_data_hash,
+        header:events as events,
+        header:gas_price::float as gas_price,
+        header:last_ds_final_block::string as last_ds_final_block,
+        header:last_final_block::string as last_final_block,
+        header:latest_protocol_version::integer as latest_protocol_version,
+        header:next_bp_hash::string as next_bp_hash,
+        header:next_epoch_id::string as next_epoch_id,
+        header:outcome_root::string as outcome_root,
+        header:prev_hash::string as prev_hash,
+        header:prev_height::integer as prev_height,
+        header:prev_state_root::string as prev_state_root,
+        header:random_value::string as random_value,
+        header:rent_paid::float as rent_paid,
+        header:signature::string as signature,
+        header:total_supply::float as total_supply,
+        header:validator_proposals as validator_proposals,
+        header:validator_reward::float as validator_reward
     from base_blocks
-    where {{ incremental_load_filter("block_timestamp") }}
+
 )
 
 select * from final
---
+++ models/staging/chainwalker_blocks.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='tx_id',
+        incremental_strategy = 'delete+insert',
+        tags=['core', 'transactions'],
+        cluster_by=['block_timestamp']
+        )
+}}
+
+with
+
+final as (
+
+    select
+        *
+    from {{ source("chainwalkers","near_blocks") }}
+    where {{ incremental_load_filter("block_timestamp") }}
+    qualify row_number() over (partition by block_id order by ingested_at desc) = 1
+
+)
+
+select * from final
