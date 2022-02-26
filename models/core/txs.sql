c5413c5124cb861b27411480df991a10b6d8b2a2
Chuxin Huang <chuxin@canva.com>
2022-02-26 21:03:43 +1100

    fixes for txs and stg_blocks

--- models/core/txs.sql
+++ models/core/txs.sql
@@ -9,17 +9,13 @@
 -- WIP -- below serves as example
 
 with base_txs as (
-    select * from {{ deduped_txs("near_txs") }}
+    select * from {{ ref("stg_txs") }}
+    where {{ incremental_load_filter("block_timestamp") }}
 ),
 final as (  
     select
-        block_timestamp,
-        tx:nonce::string as nonce,
-        tx_id as tx_hash,
-        -- Build out more columns here from `txs`
-        -- ...    
+        *    
     from base_txs
-where {{ incremental_load_filter("block_timestamp") }}
 )
 
 select * from final
