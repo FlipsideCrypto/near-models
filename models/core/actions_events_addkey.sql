13134049ae2c180a1123cc60de24be8af1081d72
forgash <jmfxyz@pm.me>
2022-03-17 11:08:14 -0600

    addkey table

---
+++ models/core/actions_events_addkey.sql
@@ -0,0 +1,35 @@
+{{
+  config(
+    materialized='table',
+    unique_key='action_id'
+  )
+}}
+
+with
+action_events as (
+
+  select * from {{ ref('actions_events') }}
+  where action_name = 'AddKey'
+
+),
+
+addkey_events as (
+
+  select
+
+    action_id,
+    tx_hash,
+    block_timestamp,
+    action_data, -- temp. to be deleted prior to merge
+    action_data:access_key:nonce::float as nonce,
+    action_data:public_key::string as public_key,
+    action_data:access_key:permission as permission,
+    action_data:access_key:permission:FunctionCall:allowance::float as allowance,
+    action_data:access_key:permission:FunctionCall:method_names as method_name,
+    action_data:access_key:permission:FunctionCall:receiver_id::string as receiver_id
+
+  from action_events
+
+)
+
+select * from addkey_events
