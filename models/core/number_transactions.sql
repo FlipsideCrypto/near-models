5826b929ca6468187ec8cf07d7b274316423d238
issui <waterflame54@gmail.com>
2022-03-24 22:26:16 -0700

    update transactions block dependency

--- models/core/number_transactions.sql
+++ models/core/number_transactions.sql
@@ -1,26 +1,25 @@
-{{ 
+{{
     config(
-        materialized='incremental', 
+        materialized='incremental',
         unique_key= 'date',
         incremental_strategy = 'delete+insert',
         tags=['core', 'transactions'],
         cluster_by = ['date']
-    ) 
+    )
 }}
 
 with n_transactions as (
 
-    select 
+    select
 
         date_trunc('day', block_timestamp) as date,
-        count(distinct tx_id) as number_of_transactions
+        count(distinct txn_hash) as number_of_transactions
 
-    from {{ ref('stg_txs') }}
+    from {{ ref('transactions') }}
     where {{ incremental_load_filter("date") }}
 
-    group by 1 
+    group by 1
 )
 
 select * from n_transactions
 order by date desc
-
