84246ea031f4bb73a054c6909242ec7396eba154
Jack Forgash <jackforgash@Jacks-MacBook-Pro.local>
2022-04-04 16:33:14 -0400

    include undecoded values for args

--- models/core/actions_events_function_call.sql
+++ models/core/actions_events_function_call.sql
@@ -2,7 +2,8 @@
   config(
     materialized='incremental',
     cluster_by='block_timestamp',
-    tags=['near','actions','events','functioncall']
+    tags=['near','actions','events','functioncall'],
+    full_refresh=true
   )
 }}
 
@@ -19,13 +20,13 @@ decoding as (
 select
 
   *,
+  action_data:args as args,
   try_base64_decode_string(action_data:args) as args_decoded,
-  action_data:deposit::float / pow(10,24) as deposit,
-  action_data:gas::float / pow(10,12) as attached_tgas,
+  action_data:deposit::number as deposit,
+  action_data:gas::number as attached_gas,
   action_data:method_name::string as method_name
 
 from action_events
-where args_decoded is not null
 
 ),
 
@@ -37,11 +38,15 @@ function_calls as (
     block_timestamp,
     action_name,
     method_name,
-    try_parse_json(args_decoded) as args,
+    case
+      when args_decoded is null then args
+      else try_parse_json(args_decoded)
+    end as args,
     deposit,
-    attached_tgas
+    attached_gas
 
   from decoding
 )
 
 select * from function_calls
+limit 1000
