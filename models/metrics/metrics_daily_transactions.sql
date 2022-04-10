c1a70059b6abbb0c111a7993637270d344f7cb3c
issui <waterflame54@gmail.com>
2022-04-09 21:22:10 -0700

    -add incremental_last_x_days macro -add metrics folder -daily transaction metrics now in metrics

--- models/metrics/metrics_daily_transactions.sql
+++ models/metrics/metrics_daily_transactions.sql
@@ -3,7 +3,7 @@
         materialized='incremental',
         unique_key= 'date',
         incremental_strategy = 'delete+insert',
-        tags=['core', 'transactions'],
+        tags=['metrics', 'transactions'],
         cluster_by = ['date']
     )
 }}
@@ -13,13 +13,12 @@ with n_transactions as (
     select
 
         date_trunc('day', block_timestamp) as date,
-        count(distinct txn_hash) as number_of_transactions
+        count(distinct txn_hash) as daily_transactions
 
     from {{ ref('transactions') }}
-    where {{ incremental_load_filter("date") }}
+    where {{ incremental_last_x_days("date", 3) }}
 
     group by 1
 )
 
 select * from n_transactions
-order by date desc
