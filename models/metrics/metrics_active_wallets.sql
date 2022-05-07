6aafafc9836209dc2b43c2ae35fce9125df37ed1
issui <waterflame54@gmail.com>
2022-05-06 20:55:11 -0700

    fixes and changes

--- models/metrics/metrics_active_wallets.sql
+++ models/metrics/metrics_active_wallets.sql
@@ -1,8 +1,6 @@
 {{
     config(
-        materialized='incremental',
-        unique_key= 'date',
-        incremental_strategy = 'delete+insert',
+        materialized='table',
         tags=['metrics'],
         cluster_by = ['date']
     )
@@ -14,11 +12,10 @@ with active_wallets as (
 
         date_trunc('day', block_timestamp) as date,
         count(distinct tx_signer) as daily_active_wallets,
-        sum(daily_active_wallets) over (order by date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) as weekly_active_wallets,
-        sum(daily_active_wallets) over (order by date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) as monthly_active_wallets
+        sum(daily_active_wallets) over (order by date rows between 6 preceding and current row) as rolling_7day_active_wallets,
+        sum(daily_active_wallets) over (order by date rows between 29 preceding and current row) as rolling_30day_active_wallets
 
     from {{ ref('transactions') }}
-    where {{ incremental_last_x_days("date", 7) }}
     group by 1
 )
 
