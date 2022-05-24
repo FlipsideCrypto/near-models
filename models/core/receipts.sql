50929eaa95dda841887e2b6d888c737b8813f1a5
issui <waterflame54@gmail.com>
2022-05-24 09:08:03 -0500

    update receipt_ids and style

--- models/core/receipts.sql
+++ models/core/receipts.sql
@@ -1,29 +1,28 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key= 'txn_hash',
-        incremental_strategy = 'delete+insert',
-        tags=['core', 'transactions'],
-        cluster_by = ['block_timestamp']
-    )
-}}
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'txn_hash',
+    incremental_strategy = 'delete+insert',
+    tags = ['core', 'transactions'],
+    cluster_by = ['block_timestamp']
+) }}
 
-select
-
+SELECT
     block_timestamp,
     block_hash,
     txn_hash,
-    tx_receipt[0]:id::string as receipt_object_id,
-
-    case when tx_receipt[0]:outcome:receipt_ids[1] is not NULL
-         then tx_receipt[0]:outcome:receipt_ids
-         else tx_receipt[0]:outcome:receipt_ids[0]::string
-    end as receipt_outcome_id,
-
-    tx_receipt[0]:outcome:status as status_value,
-    tx_receipt[0]:outcome:logs as logs,
-    tx_receipt[0]:proof as proof,
-    tx_receipt[0]:outcome:metadata as metadata
-
-from {{ ref('transactions') }}
-order by block_timestamp desc
+    VALUE :id :: STRING AS receipt_object_id,
+    CASE
+        WHEN VALUE :outcome :receipt_ids [1] IS NOT NULL THEN VALUE :outcome :receipt_ids
+        ELSE VALUE :outcome :receipt_ids [0] :: STRING
+    END AS receipt_outcome_id,
+    VALUE :outcome :status AS status_value,
+    VALUE :outcome :logs AS logs,
+    VALUE :proof AS proof,
+    VALUE :outcome :metadata AS metadata
+FROM
+    {{ ref('transactions') }},
+    LATERAL FLATTEN(
+        input => tx_receipt
+    )
+ORDER BY
+    block_timestamp DESC
