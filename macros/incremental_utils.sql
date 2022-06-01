9c210fdf9765b0b808f4b96ca58015bd93374767
Jack Forgash <jmfxyz@pm.me>
2022-06-01 11:56:41 -0600

    gaps tests and warn vs error thresholds

--- macros/incremental_utils.sql
+++ macros/incremental_utils.sql
@@ -1,16 +1,28 @@
 {% macro incremental_load_filter(time_col) -%}
-    -- dbt makes it easy to query your target table by using the "{{ this }}" variable.
-    {% if is_incremental() %}
-      {{ time_col }} > (select max({{ time_col }}) from {{ this }})
-    {%- else -%}
-      true
-    {% endif %}
+  -- dbt makes it easy to query your target table by using the "{{ this }}" variable.
+
+{% if is_incremental() %}
+{{ time_col }} > (
+  SELECT
+    MAX(
+      {{ time_col }}
+    )
+  FROM
+    {{ this }}
+)
+{%- else -%}
+  TRUE
+{% endif %}
 {%- endmacro %}
 
-{% macro incremental_last_x_days(time_col, time_in_days) -%}
-    {% if is_incremental() %}
-        {{ time_col }} >= current_date() - interval '{{ time_in_days }} day'
-    {% else %}
-        true
-    {% endif %}
+{% macro incremental_last_x_days(
+    time_col,
+    time_in_days
+  ) -%}
+
+{% if is_incremental() %}
+{{ time_col }} >= CURRENT_DATE() - INTERVAL '{{ time_in_days }} day'
+{% else %}
+  TRUE
+{% endif %}
 {%- endmacro %}
---
+++ macros/tx_gaps.sql
@@ -0,0 +1,35 @@
+{% test tx_gaps(
+    model,
+    column_name,
+    column_block,
+    column_tx_count
+) %}
+WITH block_base AS (
+    SELECT
+        {{ column_block }},
+        {{ column_tx_count }}
+    FROM
+        {{ ref('blocks') }}
+),
+model_name AS (
+    SELECT
+        {{ column_block }},
+        COUNT(
+            DISTINCT {{ column_name }}
+        ) AS model_tx_count
+    FROM
+        {{ model }}
+    GROUP BY
+        {{ column_block }}
+)
+SELECT
+    block_base.{{ column_block }},
+    {{ column_tx_count }},
+    model_name.{{ column_block }},
+    model_tx_count
+FROM
+    block_base
+    LEFT JOIN model_name
+    ON block_base.{{ column_block }} = model_name.{{ column_block }}
+WHERE
+    {{ column_tx_count }} <> model_tx_count {% endtest %}
