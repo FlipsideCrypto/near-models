e3f848304155d4d23c7d9b7620b183442c2da007
forgash_ <jmfxyz@pm.me>
2022-07-12 12:55:52 -0600

    macros and bronze

---
+++ models/bronze/bronze__blocks.sql
@@ -0,0 +1,20 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    record_id,
+    offset_id,
+    block_id,
+    block_timestamp,
+    network,
+    chain_id,
+    tx_count,
+    header,
+    ingested_at AS _ingested_at,
+    _inserted_timestamp
+FROM
+    {{ source(
+        "chainwalkers",
+        "near_blocks"
+    ) }}
---
+++ models/bronze/bronze__transactions.sql
@@ -0,0 +1,21 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    record_id,
+    tx_id,
+    tx_block_index,
+    offset_id,
+    block_id,
+    block_timestamp,
+    network,
+    chain_id,
+    tx,
+    ingested_at AS _ingested_at,
+    _inserted_timestamp
+FROM
+    {{ source(
+        "chainwalkers",
+        "near_blocks"
+    ) }}
