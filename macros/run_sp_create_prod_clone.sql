7281bf620922a0b9760c0614303f378db53ae096
Jack Forgash <jmfxyz@pm.me>
2022-07-21 11:11:57 -0600

    macro

--- macros/run_sp_create_prod_clone.sql
+++ macros/run_sp_create_prod_clone.sql
@@ -1,9 +1,9 @@
 {% macro run_sp_create_prod_clone() %}
     {% set clone_query %}
     call flow._internal.create_prod_clone(
-        'flow',
-        'flow_dev',
-        'internal_dev'
+        'near',
+        'near_dev',
+        'public'
     );
 {% endset %}
     {% do run_query(clone_query) %}
