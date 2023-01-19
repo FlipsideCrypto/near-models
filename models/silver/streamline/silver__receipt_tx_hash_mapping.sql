d3eb322c2dcaad90b53c1072532ae65db08b9412
Jack Forgash <jmfxyz@pm.me>
2023-01-18 17:18:59 -0700

    split out data receipts

--- models/silver/streamline/silver__receipt_tx_hash_mapping.sql
+++ models/silver/streamline/silver__receipt_tx_hash_mapping.sql
@@ -34,13 +34,13 @@ txs AS (
                 MAX(_partition_by_block_number)
             FROM
                 silver.streamline_receipts_final
-        ) - 10000
+        ) - 20000
         AND _partition_by_block_number <= (
             SELECT
                 MAX(_partition_by_block_number)
             FROM
                 silver.streamline_receipts_final
-        ) + 210000
+        ) + 220000
 ),
 FINAL AS (
     SELECT
