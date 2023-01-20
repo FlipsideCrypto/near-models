b352ba497abd7c1536eddc3798c2f13eb41e3552
Jack Forgash <jmfxyz@pm.me>
2023-01-20 08:42:37 -0700

    fix duplicate tag

--- models/silver/streamline/streamline__s3_sync.sql
+++ models/silver/streamline/streamline__s3_sync.sql
@@ -1,9 +1,8 @@
 {{ config(
     materialized = 'view',
-    tags = ['streamline'],
+    tags = ['streamline', 's3_copy'],
     post_hook = "select * from {{this.schema}}.{{this.identifier}}",
-    comment = "incrementally sync Streamline.near_dev.blocks and Streamline.near_dev.shards",
-    tags = ['s3_copy']
+    comment = "incrementally sync Streamline.near_dev.blocks and Streamline.near_dev.shards"
 ) }}
 
 WITH latest_block AS (
