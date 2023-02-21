e7e43a83207e1e298b45069be8e702c49e36ecd6
Jack Forgash <jmfxyz@pm.me>
2023-02-21 09:41:00 -0700

    social receipt filter

---
+++ models/social/social__receipts.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'action_id',
+    cluster_by = ['_load_timestamp::date', '_partition_by_block_number'],
+    tags = ['s3_curated']
+) }}
+
+WITH receipts AS (
+
+    SELECT
+        *
+    FROM
+        {{ ref('silver__streamline_receipts_final') }}
+    WHERE
+        {% if target.name == 'manual_fix' or target.name == 'manual_fix_dev' %}
+            {{ partition_load_manual('no_buffer') }}
+        {% else %}
+            {{ incremental_load_filter('_load_timestamp') }}
+        {% endif %}
+
+        AND (
+            LOWER(signer_id) = 'social.near'
+            OR LOWER(receiver_id) = 'social.near'
+            )
+)
+SELECT
+    *
+FROM
+    receipts
