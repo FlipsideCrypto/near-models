97aecf674d18c1d168f6e4268be6147bc4dd729e
Jack Forgash <jmfxyz@pm.me>
2023-06-02 08:17:22 -0600

    stage changes

--- models/silver/atlas/silver__lockup_account_balances.py
+++ models/silver/atlas/silver__lockup_account_balances.py
@@ -1,33 +1,25 @@
 import snowflake.snowpark.types as T
 import snowflake.snowpark.functions as F
-from datetime import datetime
 
-def register_udf_try_call_api():
-    """
-    A wrapper UDF on the UDF_API to add a try/except block to catch and log errors.
-    """
-    try_call_api = (
-        F.udf(
-            # lambda method, url, headers, data: F.try_(F.call_udf('ethereum.streamline.udf_api', method, url, headers, data)).catch_(lambda e: {'error': str(e)}),
-            lambda method, url, headers, data: F.call_udf('ethereum.streamline.udf_api', method, url, headers, data),
-            name='try_call_api',
-            input_types=[
-                T.StringType(),
-                T.StringType(),
-                T.VariantType(),
-                T.VariantType()
-            ],
-            return_type=T.VariantType(),
-            if_not_exists=True
-        )
-    )
 
-    return try_call_api
+def try_call_udf_api(method, url, headers, data):
+    """
+    A wrapper function on the UDF_API to add a try/except block to catch and log errors.
+    """
+
+    try:
+        res = F.call_udf('ethereum.streamline.udf_api', method, url, headers, data)
+
+    except Exception as e:
+        res = {'error': str(e)}
+
+    return res
+
 
 
 def register_udf_construct_url():
     """
-    Helper function to register a UDF to construct the URL for the API call.
+    Helper function to register an anonymous UDF to construct the URL for the API call.
     This particular URL requires 2 inputs from another table, account_id and block_id, with are passed in as both a URL path and parameter.
     The inputs will be passed to the UDF, not to the registration function, thus the function has no arguments while the UDF has 3, as indicated by the array input_types.
     """
@@ -62,134 +54,32 @@ def request(session, base_url, api_key=None, df=None):
     }
     data = {}
 
-    # instantiate object for helper udfs
+    # instantiate object for helper udf(s)
     construct_url = register_udf_construct_url()
-    # try_call_api = register_udf_try_call_api()
 
-    # define schema for response df
-    # schema = T.StructType(
-    #         [
-    #             T.StructField('BLOCK_DATE', T.DateType()),
-    #             T.StructField('BLOCK_ID', T.IntegerType()),
-    #             T.StructField('LOCKUP_ACCOUNT_ID', T.StringType()),
-    #             T.StructField('RESPONSE', T.VariantType()),
-    #             T.StructField('_REQUEST_TIMESTAMP', T.TimestampType()),
-    #             T.StructField('_RES_ID', T.StringType())
-    #         ]
-    #     )
-
-    # create empty df to store response
-    # response_df = session.create_dataframe([], schema)
-
-    # loop through df and call api via udf
-    # TODO - change to request batch of N at a time, instead of 1 by 1
-
-    error_count = 0
-
-    # TODO land udf in near db
-    response_df = df.with_column(
-        "RESPONSE",
-        # F.call_udf(
-        #     'try_call_api',
-        #     method,
-        #     construct_url(
-        #         F.lit(base_url),
-        #         F.col('LOCKUP_ACCOUNT_ID'),
-        #         F.col('BLOCK_ID')
-        #     ),
-        #     headers,
-        #     data
-        # )
-    # try_call_api(
-    #     method,
-    #     construct_url(
-    #         F.lit(base_url),
-    #         F.col('LOCKUP_ACCOUNT_ID'),
-    #         F.col('BLOCK_ID')
-    #     ),
-    #     headers,
-    #     data
-    # )
-    F.call_udf(
-        'ethereum.streamline.udf_api', 
-        method, 
-        construct_url(
-            F.lit(base_url),
-            F.col('LOCKUP_ACCOUNT_ID'),
-            F.col('BLOCK_ID')
-        ), 
-        headers, 
-        data
-        )
-    ).with_column(
-        "_REQUEST_TIMESTAMP",
-        F.current_timestamp()
-    ).with_column(
-        "_RES_ID",
-        F.concat_ws(
-            F.lit('-'),
-            F.col('LOCKUP_ACCOUNT_ID'),
-            F.col('BLOCK_ID')
-        )
+    response_df = df.with_columns(
+        ["RESPONSE", "_REQUEST_TIMESTAMP", "_RES_ID"],
+        [
+            try_call_udf_api(
+                method,
+                construct_url(
+                    F.lit(base_url),
+                    F.col('LOCKUP_ACCOUNT_ID'),
+                    F.col('BLOCK_ID')
+            ),
+            headers,
+            data
+            ),
+            F.current_timestamp(),
+            F.concat_ws(
+                F.lit('-'),
+                F.col('LOCKUP_ACCOUNT_ID'),
+                F.col('BLOCK_ID')
+            )
+        ]
     )
-    # for block_id in df.select('BLOCK_ID', 'BLOCK_DATE').distinct().order_by('BLOCK_ID').collect():
-        
-    #     for account_id in df.select('LOCKUP_ACCOUNT_ID').where(f"BLOCK_ID = {block_id['BLOCK_ID']}").collect():
-            
-    #         url = base_url.replace(
-    #                 "{account_id}", account_id['LOCKUP_ACCOUNT_ID']
-    #             ).replace(
-    #                 "{block_id}", str(block_id['BLOCK_ID'])
-    #             )
-
-    #         sql = f"""
-    #             select
-    #                 '{block_id['BLOCK_DATE']}'::DATE as BLOCK_DATE,
-    #                 {block_id['BLOCK_ID']}::INT as BLOCK_ID,
-    #                 '{account_id['LOCKUP_ACCOUNT_ID']}' as LOCKUP_ACCOUNT_ID,
-    #                 ethereum.streamline.udf_api(
-    #                     '{method}',
-    #                     '{url}',
-    #                     {headers},
-    #                     {data}
-    #                 ) as RESPONSE,
-    #                 CURRENT_TIMESTAMP as _REQUEST_TIMESTAMP,
-    #                 CONCAT_WS('-', LOCKUP_ACCOUNT_ID, BLOCK_ID) as _RES_ID
-    #         """
-
-    #         try:
-    #             # execute sql via collect() and append to response
-    #             r = session.sql(sql).collect()
-    #             response_df = response_df.union(session.createDataFrame(r, schema))
-
-    #         except Exception as e:
-    #             error_count += 1
-
-    #             # log error in table
-    #             response_df = response_df.union(
-    #                 session.create_dataframe(
-    #                     [
-    #                         {
-    #                             'BLOCK_DATE': block_id['BLOCK_DATE'],
-    #                             'BLOCK_ID': block_id['BLOCK_ID'], 
-    #                             'LOCKUP_ACCOUNT_ID': account_id['LOCKUP_ACCOUNT_ID'], 
-    #                             'RESPONSE': {
-    #                                     'error': str(e)
-    #                                 },
-    #                             '_REQUEST_TIMESTAMP': datetime.now(),
-    #                             '_RES_ID': f"{account_id['LOCKUP_ACCOUNT_ID']}-{block_id['BLOCK_ID']}"
-    #                         }
-    #                     ],
-    #                     schema
-    #                 )
-    #             )
-
-    #             # arbitrary limit of 10 errors
-    #             if error_count >= 10:
-    #                 raise Exception(f"Too many errors - {error_count}")
-
+    
     return response_df
-    # pass
 
 
 def model(dbt, session):
@@ -240,6 +130,11 @@ def model(dbt, session):
     base_url = 'https://near-mainnet.api.pagoda.co/eapi/v1/accounts/{account_id}/balances/NEAR?block_height={block_id}'
     api_key = session.sql("select * from near._internal.api_key where platform = 'pagoda'").collect()[0]['API_KEY']
     df = active_accounts
+
+
+    # TODO - add error threshold
+    error_count = 0
+
 
     final_df = request(
         session,
