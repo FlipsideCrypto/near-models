419172ec85f8bf64ee1cea874afc96ca911d5afc
Jack Forgash <jmfxyz@pm.me>
2023-06-02 08:58:43 -0600

    dual request option

--- models/silver/atlas/silver__lockup_account_balances.py
+++ models/silver/atlas/silver__lockup_account_balances.py
@@ -1,20 +1,7 @@
 import snowflake.snowpark.types as T
 import snowflake.snowpark.functions as F
-
-
-def try_call_udf_api(method, url, headers, data):
-    """
-    A wrapper function on the UDF_API to add a try/except block to catch and log errors.
-    """
-
-    try:
-        res = F.call_udf('ethereum.streamline.udf_api', method, url, headers, data)
-
-    except Exception as e:
-        res = {'error': str(e)}
-
-    return res
-
+from datetime import datetime
+# import logging
 
 
 def register_udf_construct_url():
@@ -40,7 +27,7 @@ def register_udf_construct_url():
     return construct_url
 
 
-def request(session, base_url, api_key=None, df=None):
+def batch_request(session, base_url, api_key=None, df=None):
     """
     Function to call the UDF_API.
     df (optional) - Snowpark DataFrame of input data
@@ -60,15 +47,16 @@ def request(session, base_url, api_key=None, df=None):
     response_df = df.with_columns(
         ["RESPONSE", "_REQUEST_TIMESTAMP", "_RES_ID"],
         [
-            try_call_udf_api(
+            F.call_udf(
+                'ethereum.streamline.udf_api',
                 method,
                 construct_url(
                     F.lit(base_url),
                     F.col('LOCKUP_ACCOUNT_ID'),
                     F.col('BLOCK_ID')
-            ),
-            headers,
-            data
+                ),
+                headers,
+                data
             ),
             F.current_timestamp(),
             F.concat_ws(
@@ -79,6 +67,98 @@ def request(session, base_url, api_key=None, df=None):
         ]
     )
     
+    return response_df
+
+
+def loop_request(session, base_url, api_key=None, df=None):
+    """
+    Call the UDF_API in a loop, one call at a time, to isolate and log errors as JSON responses, instead of raising an exception.
+    """
+
+    # define params for UDF_API
+    method = 'GET'
+    headers = {
+        'Content-Type': 'application/json',
+        'x-api-key': api_key
+    }
+    data = {}
+
+    # instantiate object for helper udf(s)
+    construct_url = register_udf_construct_url()
+
+    error_count = 0
+
+    # build response_df
+    schema = T.StructType([
+        T.StructField('BLOCK_DATE', T.DateType()),
+        T.StructField('BLOCK_ID', T.IntegerType()),
+        T.StructField('LOCKUP_ACCOUNT_ID', T.StringType()),
+        T.StructField('RESPONSE', T.VariantType()),
+        T.StructField('_REQUEST_TIMESTAMP', T.TimestampType()),
+        T.StructField('_RES_ID', T.StringType())
+    ])
+
+    response_df = session.create_dataframe([], schema)
+
+    for block_id in df.select('BLOCK_ID', 'BLOCK_DATE').distinct().order_by('BLOCK_ID').collect():
+        
+        for account_id in df.select('LOCKUP_ACCOUNT_ID').where(f"BLOCK_ID = {block_id['BLOCK_ID']}").collect():
+            
+            # TODO - swap with construct_url
+            url = base_url.replace(
+                    "{account_id}", account_id['LOCKUP_ACCOUNT_ID']
+                ).replace(
+                    "{block_id}", str(block_id['BLOCK_ID'])
+                )
+
+            # TODO constuct sql UDF? or is this fine?
+            sql = f"""
+                select
+                    '{block_id['BLOCK_DATE']}'::DATE as BLOCK_DATE,
+                    {block_id['BLOCK_ID']}::INT as BLOCK_ID,
+                    '{account_id['LOCKUP_ACCOUNT_ID']}' as LOCKUP_ACCOUNT_ID,
+                    ethereum.streamline.udf_api(
+                        '{method}',
+                        '{url}',
+                        {headers},
+                        {data}
+                    ) as RESPONSE,
+                    CURRENT_TIMESTAMP as _REQUEST_TIMESTAMP,
+                    CONCAT_WS('-', LOCKUP_ACCOUNT_ID, BLOCK_ID) as _RES_ID
+            """
+
+            try:
+                # execute sql via collect() and append to response
+                r = session.sql(sql).collect()
+                response_df = response_df.union(session.createDataFrame(r, schema))
+
+            except Exception as e:
+                error_count += 1
+
+                # log error in table
+                response_df = response_df.union(
+                    session.create_dataframe(
+                        [
+                            {
+                                'BLOCK_DATE': block_id['BLOCK_DATE'],
+                                'BLOCK_ID': block_id['BLOCK_ID'], 
+                                'LOCKUP_ACCOUNT_ID': account_id['LOCKUP_ACCOUNT_ID'], 
+                                'RESPONSE': {
+                                        'error': str(e)
+                                    },
+                                '_REQUEST_TIMESTAMP': datetime.now(),
+                                '_RES_ID': f"{account_id['LOCKUP_ACCOUNT_ID']}-{block_id['BLOCK_ID']}"
+                            }
+                        ],
+                        schema
+                    )
+                )
+
+                # arbitrary limit of 10 errors
+                if error_count >= 10:
+                    # raise exception and break loop at threshold
+                    raise Exception(f"Too many errors - {error_count}")
+
     return response_df
 
 
@@ -131,17 +211,31 @@ def model(dbt, session):
     api_key = session.sql("select * from near._internal.api_key where platform = 'pagoda'").collect()[0]['API_KEY']
     df = active_accounts
 
+    # TODO - implement batches here with a .limit() & loop
+    batch_size = 25
 
-    # TODO - add error threshold
-    error_count = 0
+    try:
+        final_df = batch_request(
+            session,
+            base_url,
+            api_key,
+            df
+        )
 
+        # batch_request returns a df which is not executed until dbt executes and attempts to write to a table
+        # initiate execution to any error within the batch, and kick-off a loop through the batch instead
+        execute = final_df.collect()
 
-    final_df = request(
-        session,
-        base_url,
-        api_key,
-        df
-    )
+    except Exception as e:
+        # if above fails, call each row individually
+        # it is considerably slower, but allows for logging errors (on the individual call(s) that failed) instead of the job failing
+        
+        final_df = loop_request(
+            session, 
+            base_url,
+            api_key,
+            df
+        )
 
     # dbt models return a df which is written as a table
     return final_df
