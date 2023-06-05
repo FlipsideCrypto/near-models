aeb49bd2259d5dba43f94a2a511cfbbbd0814e3b
Jack Forgash <jmfxyz@pm.me>
2023-06-05 16:33:08 -0600

    create utils

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -4,7 +4,7 @@
             {% set sql %}
             CREATE schema if NOT EXISTS silver;
 CREATE schema if NOT EXISTS streamline;
-{{ create_udf_introspect() }}
+            {{ create_udf_introspect() }}
             {{ create_udf_s3_list_directories() }}
             {{ create_udf_s3_list_objects() }}
             {{ create_udf_s3_copy_objects() }}
@@ -12,6 +12,12 @@ CREATE schema if NOT EXISTS streamline;
 
             {% endset %}
             {% do run_query(sql) %}
+
+        {% set utils_udfs %}
+        {{- fsc_utils.create_udfs() -}}
+        {% endset %}
+        {% do run_query(utils_udfs) %}
+
         {% endif %}
     {% endif %}
 {% endmacro %}
