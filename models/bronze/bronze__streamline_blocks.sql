32d88d689b01cc8a88e36a631bbb7f5375869209
Jack Forgash <jmfxyz@pm.me>
2023-07-25 21:09:04 -0600

    add meta to bronze

--- models/bronze/bronze__streamline_blocks.sql
+++ models/bronze/bronze__streamline_blocks.sql
@@ -2,17 +2,62 @@
     materialized = 'view'
 ) }}
 
-SELECT
-    metadata$filename AS _filename,
-    SPLIT(
-        _filename,
-        '/'
-    ) [0] :: NUMBER AS block_id,
-    CURRENT_TIMESTAMP :: TIMESTAMP_NTZ as _load_timestamp,
-    VALUE,
-    _partition_by_block_number
-FROM
-    {{ source(
-        "streamline_dev",
-        "blocks"
-    ) }}
+WITH external_blocks AS (
+
+    SELECT
+        metadata$filename AS _filename,
+        SPLIT(
+            _filename,
+            '/'
+        ) [0] :: NUMBER AS block_id,
+        CURRENT_TIMESTAMP :: timestamp_ntz AS _load_timestamp,
+        VALUE,
+        _partition_by_block_number
+    FROM
+        {{ source(
+            "streamline_dev",
+            "blocks"
+        ) }}
+),
+meta AS (
+    SELECT
+        job_created_time AS _inserted_timestamp,
+        file_name AS _filename,
+        SPLIT(
+            _filename,
+            '/'
+        ) [0] :: NUMBER AS block_id,
+        FLOOR(
+            block_id,
+            -4
+        ) AS _partition_by_block_number
+    FROM
+        TABLE(
+            information_schema.external_table_file_registration_history(
+                start_time => DATEADD('day', -3, CURRENT_TIMESTAMP()),
+                table_name => '{{ source( 'streamline_dev', 'blocks' ) }}')
+            ) A
+        ),
+        FINAL AS (
+            SELECT
+                e._filename,
+                e.block_id,
+                IFF(
+                    m._inserted_timestamp IS NULL,
+                    e._load_timestamp,
+                    NULL
+                ) AS _load_timestamp,
+                e.value,
+                e._partition_by_block_number,
+                m._inserted_timestamp
+            FROM
+                external_blocks e
+                LEFT JOIN meta m USING (
+                    block_id,
+                    _partition_by_block_number
+                )
+        )
+    SELECT
+        *
+    FROM
+        FINAL
--- models/bronze/bronze__streamline_shards.sql
+++ models/bronze/bronze__streamline_shards.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = 'view'
 ) }}
-
+with external_shards as (
 SELECT
     metadata$filename AS _filename,
     SPLIT(
@@ -17,3 +17,47 @@ FROM
         "streamline_dev",
         "shards"
     ) }}
+),
+meta AS (
+    SELECT
+        job_created_time AS _inserted_timestamp,
+        file_name AS _filename,
+        SPLIT(
+            _filename,
+            '/'
+        ) [0] :: NUMBER AS block_id,
+        FLOOR(
+            block_id,
+            -4
+        ) AS _partition_by_block_number
+    FROM
+        TABLE(
+            information_schema.external_table_file_registration_history(
+                start_time => DATEADD('day', -3, CURRENT_TIMESTAMP()),
+                table_name => '{{ source( 'streamline_dev', 'shards' ) }}')
+            ) A
+        ),
+        FINAL AS (
+            SELECT
+                e._filename,
+                e.block_id,
+                IFF(
+                    m._inserted_timestamp IS NULL,
+                    e._load_timestamp,
+                    NULL
+                ) AS _load_timestamp,
+                e._shard_number,
+                e.value,
+                e._partition_by_block_number,
+                m._inserted_timestamp
+            FROM
+                external_shards e
+                LEFT JOIN meta m USING (
+                    block_id,
+                    _partition_by_block_number
+                )
+        )
+    SELECT
+        *
+    FROM
+        FINAL
