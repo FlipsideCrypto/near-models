bfad3d7a46ba74422a58b5bdee8275e59fee5b28
Jack Forgash <jmfxyz@pm.me>
2023-08-01 10:17:44 -0600

    add 1hr buffer to gap

--- tests/tests__chunk_gaps.sql
+++ tests/tests__chunk_gaps.sql
@@ -16,6 +16,7 @@ WITH block_chunks_included AS (
 chunks_per_block AS (
     SELECT
         block_id,
+        MAX(_inserted_timestamp) AS _inserted_timestamp,
         COUNT(
             DISTINCT chunk_hash
         ) AS chunk_ct
@@ -30,7 +31,9 @@ comp AS (
         b.block_id AS bblock_id,
         C.block_id AS cblock_id,
         b.chunks_included,
-        C.chunk_ct
+        C.chunk_ct,
+        b._inserted_timestamp AS b_inserted_timestamp,
+        C._inserted_timestamp AS c_inserted_timestamp
     FROM
         block_chunks_included b full
         OUTER JOIN chunks_per_block C USING (block_id)
@@ -47,6 +50,8 @@ missing AS (
             OR cblock_id IS NULL
             OR chunks_included != chunk_ct
         )
+        AND b_inserted_timestamp <= CURRENT_TIMESTAMP - INTERVAL '1 hour'
+        AND c_inserted_timestamp <= CURRENT_TIMESTAMP - INTERVAL '1 hour'
     ORDER BY
         1
 )
