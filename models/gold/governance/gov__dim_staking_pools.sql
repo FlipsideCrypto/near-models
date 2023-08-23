64063a7dffdeca39d6f59f0a28113704646111a2
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-08-23 11:38:04 +0200

    docs, new views, crosschain's table update

---
+++ models/gold/governance/gov__dim_staking_pools.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized = 'view',
+    secure = true,
+    tags = ['core', 'governance'],
+    meta={
+        'database_tags':{
+            'table': {
+                'PURPOSE': 'STAKING GOVERNANCE'
+            }
+        }
+    }
+) }}
+
+WITH staking_pools AS (
+
+    SELECT
+        *
+    FROM
+        {{ ref('silver__staking_pools_s3') }}
+)
+SELECT
+    tx_hash,
+    block_timestamp,
+    owner,
+    address,
+    reward_fee_fraction,
+    tx_type
+FROM
+    staking_pools
---
+++ models/gold/governance/gov__fact_staking_pool_daily_balances.sql
@@ -0,0 +1,26 @@
+{{ config(
+    materialized = 'view',
+    secure = true,
+    tags = ['core', 'governance'],
+    meta={
+        'database_tags':{
+            'table': {
+                'PURPOSE': 'STAKING GOVERNANCE'
+            }
+        }
+    }
+) }}
+
+WITH daily_balance AS (
+
+    SELECT
+        date_day as date,
+        address,
+        balance
+    FROM
+        {{ ref('silver__pool_balance_daily') }}
+)
+SELECT
+    *
+FROM
+    daily_balance
