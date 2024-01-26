acedd585de71263c8eb073cd719672406a4dc23f
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-01-26 14:34:31 +0700

    update labels

--- models/gold/core/core__dim_address_labels.sql
+++ models/gold/core/core__dim_address_labels.sql
@@ -17,12 +17,7 @@ WITH flipside_labels AS (
         l1_label,
         l2_label,
         creator,
-        COALESCE(
-            address_labels_id,
-            {{ dbt_utils.generate_surrogate_key(
-                ['address']
-            ) }}
-        ) AS dim_address_labels_id,
+        address_labels_id AS dim_address_labels_id,
         COALESCE(inserted_timestamp, _inserted_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) AS inserted_timestamp,
         COALESCE(modified_timestamp, _inserted_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) AS modified_timestamp
     FROM
