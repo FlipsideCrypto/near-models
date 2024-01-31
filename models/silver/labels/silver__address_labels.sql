7a3c9b1c9272d8525670001c2d66eecbbd9b137e
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-01-31 20:37:34 +0100

    delete private mod

--- models/silver/labels/silver__address_labels.sql
+++ models/silver/labels/silver__address_labels.sql
@@ -22,14 +22,14 @@ WITH address_labels AS (
         _is_deleted,
         _load_timestamp,
         _inserted_timestamp,
-        _modified_timestamp
+        modified_timestamp
         
     FROM
         {{ ref('bronze__address_labels') }}
 
 {% if is_incremental() %}
 WHERE
-    {{ incremental_load_filter('_modified_timestamp') }}
+    {{ incremental_load_filter('modified_timestamp') }}
 {% endif %}
 )
 SELECT
@@ -49,7 +49,6 @@ SELECT
     _is_deleted,
     labels_combined_id  as address_labels_id,
     SYSDATE() AS inserted_timestamp,
-    SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
     address_labels
