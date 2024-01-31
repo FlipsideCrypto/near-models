f1fadbcbabe22b44f43330fbc18c0b7201f1bddf
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-01-31 10:54:50 +0100

    remove colasce

--- models/gold/core/core__dim_address_labels.sql
+++ models/gold/core/core__dim_address_labels.sql
@@ -18,8 +18,8 @@ WITH flipside_labels AS (
         l2_label,
         creator,
         address_labels_id AS dim_address_labels_id,
-        COALESCE(inserted_timestamp, _inserted_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) AS inserted_timestamp,
-        COALESCE(modified_timestamp, _inserted_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) AS modified_timestamp
+        inserted_timestamp,
+        modified_timestamp
     FROM
         {{ ref('silver__address_labels') }}
 )
