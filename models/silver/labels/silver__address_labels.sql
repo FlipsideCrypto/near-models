111c19cdca3a2a77787cc6c25d224e1e230043e9
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-01-31 20:18:26 +0100

    hotfix

--- models/silver/labels/silver__address_labels.sql
+++ models/silver/labels/silver__address_labels.sql
@@ -14,39 +14,28 @@ WITH address_labels AS (
         blockchain,
         address,
         creator,
-        label_type,
-        label_subtype,
         address_name,
         project_name,
-        labels_combined_id,
-        _is_deleted,
+        label_type,
+        label_subtype,
+        label_type AS l1_label,
+        label_subtype AS l2_label,
         _load_timestamp,
-        _inserted_timestamp
+        _inserted_timestamp,
+        _is_deleted,
+        labels_combined_id  as address_labels_id,
+        SYSDATE() AS inserted_timestamp,
+        SYSDATE() AS modified_timestamp,
+        '{{ invocation_id }}' AS _invocation_id
+
     FROM
         {{ ref('bronze__address_labels') }}
-
-{% if is_incremental() %}
-WHERE
-    {{ incremental_load_filter('modified_timestamp') }}
-{% endif %}
+    {% if is_incremental() %}
+    WHERE
+        {{ incremental_load_filter('modified_timestamp') }}
+    {% endif %}
 )
 SELECT
-    system_created_at,
-    blockchain,
-    address,
-    creator,
-    address_name,
-    project_name,
-    label_type,
-    label_subtype,
-    label_type AS l1_label,
-    label_subtype AS l2_label,
-    _load_timestamp,
-    _inserted_timestamp,
-    _is_deleted,
-    labels_combined_id  as address_labels_id,
-    SYSDATE() AS inserted_timestamp,
-    SYSDATE() AS modified_timestamp,
-    '{{ invocation_id }}' AS _invocation_id
+    *
 FROM
     address_labels
