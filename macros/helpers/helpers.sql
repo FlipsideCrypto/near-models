10b56ac79bd79d3d9f6f27c5a498bba72daff861
WHYTEWYLL <juanmiblanco1@gmail.com>
2024-04-12 12:00:49 +0300

    macro

---
+++ macros/helpers/helpers.sql
@@ -0,0 +1,10 @@
+{% macro dispatch_github_workflow(repo_name, workflow_name) %}
+    {% set context_query %}
+        SET LIVEQUERY_CONTEXT = '{"userId":"{{ var("GB_ID") }}"}';
+    {% endset %}
+    {% do run_query(context_query) %}
+    {% set query %}
+        SELECT github_actions.workflow_dispatches('FlipsideCrypto', '{{ repo_name }}', '{{ workflow_name }}.yml', NULL)
+    {% endset %}
+    {% do run_query(query) %}
+{% endmacro %}
