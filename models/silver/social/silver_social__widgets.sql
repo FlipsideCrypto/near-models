c0aab5a0471d1cf49f241639da67d54677c3307d
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-07-24 11:57:40 -0400

    rm pred for non-clustered tables, add block cluster for blocks

--- models/silver/social/silver_social__widgets.sql
+++ models/silver/social/silver_social__widgets.sql
@@ -1,6 +1,5 @@
 {{ config(
     materialized = 'incremental',
-    incremental_predicates = ["COALESCE(DBT_INTERNAL_DEST.block_timestamp::DATE,'2099-12-31') >= (select min(block_timestamp::DATE) from " ~ generate_tmp_view_name(this) ~ ")"],
     merge_exclude_columns = ["inserted_timestamp"],
     unique_key = 'social_widgets_id',
     cluster_by = ['_inserted_timestamp::date', '_partition_by_block_number'],
