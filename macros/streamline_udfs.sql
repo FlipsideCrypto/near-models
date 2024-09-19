11c304176586541a21a54dd96b09373525bb8a16
shah <info@shahnewazkhan.ca>
2024-09-19 09:30:12 -0700

    fix udf macro names

--- macros/streamline_udfs.sql
+++ macros/streamline_udfs.sql
@@ -63,7 +63,7 @@
 
 {% macro create_udf_decode_withrawal_event() %}
     CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_decode_withrawal_event(
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_decode_near_event(
         DATA STRING
     ) returns ARRAY api_integration = aws_near_api_stg_v2 AS {% if target.name == "prod" %}
         ''
