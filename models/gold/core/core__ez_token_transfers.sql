9ad61bb811e3c6474afd9e7c4dbb24bead7d90ba
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-10-02 16:37:33 -0600

    upd ez transfers price lookback

--- models/gold/core/core__ez_token_transfers.sql
+++ models/gold/core/core__ez_token_transfers.sql
@@ -2,34 +2,52 @@
     materialized = 'incremental',
     unique_key = "ez_token_transfers_id",
     incremental_strategy = 'delete+insert',
-    cluster_by = ['block_timestamp::DATE'],
+    cluster_by = ['block_timestamp::DATE', 'floor(block_id, -3)'],
     tags = ['core']
 ) }}
 
-WITH hourly_prices AS (
+{% if execute %}
+    {% if is_incremental() %}
+        {% set query %}
 
-    SELECT
-        token_address,
-        price,
-        HOUR,
-        modified_timestamp
-    FROM
-        {{ ref('price__ez_prices_hourly') }}
-
-        {% if var('MANUAL_FIX') %}
-        -- Note, this will intentionally fail if a date range is not passed
+        SELECT
+            MIN(DATE_TRUNC('day', block_timestamp)) AS block_timestamp_day
+        FROM
+            {{ ref('silver__token_transfers_complete') }}
         WHERE
-            DATE_TRUNC(
-                'day',
-                HOUR
-            ) BETWEEN '{{ var('RANGE_START_DATE', None) }}'
-            AND '{{ var('RANGE_END_DATE', None) }}'
-        {% else %}
-
-{% if is_incremental() %}
-WHERE
-    HOUR >= DATEADD(DAY, -1, SYSDATE())
+            modified_timestamp >= (
+                SELECT
+                    MAX(modified_timestamp)
+                FROM
+                    {{ this }}
+            ) 
+            {% endset %}
+            {% set min_block_timestamp_day = run_query(query).columns [0].values() [0] %}
+    {% elif var('MANUAL_FIX') %}
+            {% set query %}
+            SELECT
+                MIN(DATE_TRUNC('day', block_timestamp)) AS block_timestamp_day
+            FROM
+                {{ this }}
+            WHERE
+                FLOOR(block_id, -3) = {{ var('RANGE_START') }}
+            {% endset %}
+            {% set min_block_timestamp_day = run_query(query).columns [0].values() [0] %}
+    {% endif %}
 {% endif %}
+
+    WITH hourly_prices AS (
+        SELECT
+            token_address,
+            price,
+            HOUR,
+            modified_timestamp
+        FROM
+            {{ ref('price__ez_prices_hourly') }}
+
+{% if is_incremental() or var('MANUAL_FIX') %}
+WHERE
+    date_trunc('day', HOUR) >= '{{ min_block_timestamp_day }}'
 {% endif %}
 
 qualify(ROW_NUMBER() over (PARTITION BY token_address, HOUR
@@ -79,27 +97,26 @@ FROM
         t.block_timestamp
     ) = HOUR
     LEFT JOIN {{ ref('silver__ft_contract_metadata') }} C USING (contract_address) 
-    
     {% if var("MANUAL_FIX") %}
     WHERE
         {{ partition_load_manual('no_buffer') }}
     {% else %}
 
-        {% if is_incremental() %}
-        WHERE
-            GREATEST(
-                t.modified_timestamp,
-                '2000-01-01'
-            ) >= DATEADD(
-                'minute',
-                -5,(
-                    SELECT
-                        MAX(
-                            modified_timestamp
-                        )
-                    FROM
-                        {{ this }}
+{% if is_incremental() %}
+WHERE
+    GREATEST(
+        t.modified_timestamp,
+        '2000-01-01'
+    ) >= DATEADD(
+        'minute',
+        -5,(
+            SELECT
+                MAX(
+                    modified_timestamp
                 )
-            )
-        {% endif %}
-    {% endif %}
+            FROM
+                {{ this }}
+        )
+    )
+{% endif %}
+{% endif %}
