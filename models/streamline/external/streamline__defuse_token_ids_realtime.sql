1beac894376b4f4bff438134c20a7d2d9e6ad5a0
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-03-04 10:36:05 -0700

    defuse labels

--- models/streamline/external/streamline__defuse_token_ids_realtime.sql
+++ models/streamline/external/streamline__defuse_token_ids_realtime.sql
@@ -1,16 +1,39 @@
 {{ config (
     materialized = "view",
-    post_hook = fsc_utils.if_data_call_function_v2(
-        func = 'streamline.udf_bulk_rest_api_v2',
-        target = "{{this.schema}}.{{this.identifier}}",
-        params = {
-            "external_table": "defuse_token_ids",
-            "sql_limit": "10000",
-            "producer_batch_size": "5000",
-            "worker_batch_size": "2500",
-            "sql_source": "{{this.identifier}}",
-            "order_by_column": "block_id DESC"
-        }
-    ),
     tags = ['streamline_realtime_noncore']
 ) }}
+
+WITH api_call AS (
+
+    SELECT
+        {{ target.database }}.live.udf_api(
+            'POST',
+            'https://bridge.chaindefuser.com/rpc',
+            OBJECT_CONSTRUCT(
+                'Content-Type',
+                'application/json',
+                'fsc-quantum-state',
+                'livequery'
+            ),
+            OBJECT_CONSTRUCT(
+                'id',
+                'dontcare',
+                'jsonrpc',
+                '2.0',
+                'method',
+                'supported_tokens',
+                'params',
+                []
+            )
+        ) :: variant AS response
+)
+SELECT
+    response
+FROM
+    api_call
+WHERE
+    response IS NOT NULL
+    AND response :status_code :: INT IN (
+        200,
+        201
+    )
