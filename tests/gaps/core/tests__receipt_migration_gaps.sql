eebd710e18ad72f2057a1a559496b6e1c99736b4
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-03-10 13:25:32 -0600

    test - migration receipt ct

---
+++ tests/gaps/core/tests__receipt_migration_gaps.sql
@@ -0,0 +1,35 @@
+{{ config(
+    severity = 'error',
+    tags = ['gap_test']
+) }}
+
+WITH 
+archive AS (
+    select
+        floor(block_id, -6) as block_group,
+        count(distinct receipt_id) as receipt_count,
+        count(distinct block_id) as block_count
+    from {{ ref('silver__streamline_receipts') }}
+    where floor(block_id, -6) between {{ var('start_block') }} and {{ var('end_block') }}
+    group by 1
+),
+destination AS (
+    select
+        floor(block_id, -6) as block_group,
+        count(distinct receipt_id) as receipt_count,
+        count(distinct block_id) as block_count
+    from {{ ref('silver__receipts_final') }}
+    where floor(block_id, -6) between {{ var('start_block') }} and {{ var('end_block') }}
+    group by 1
+)
+select
+    archive.block_group,
+    archive.receipt_count as receipt_ct_expected,
+    archive.block_count as block_ct_expected,
+    destination.receipt_count as receipt_ct_actual,
+    destination.block_count as block_ct_actual
+from archive
+left join destination
+on archive.block_group = destination.block_group
+where receipt_ct_expected <> receipt_ct_actual
+order by block_group
