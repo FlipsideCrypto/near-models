ff941479e6d35ce022b3b05206aa3b6c086c1da6
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-03-19 09:30:01 -0600

    rm typo

--- models/gold/defi/defi__fact_intents.sql
+++ models/gold/defi/defi__fact_intents.sql
@@ -109,7 +109,7 @@ logs_base AS(
 
     {% if var("MANUAL_FIX") %}
         AND
-            {{ partition_load_manual('no_buffer', floor) }}
+            {{ partition_load_manual('no_buffer') }}
         {% else %}
         {% if is_incremental() %}
             AND block_timestamp::DATE >= '{{min_bd}}'
