a2b58c9f94981fffe6b783f2ac3f8eb4317ada95
shah <info@shahnewazkhan.ca>
2025-04-27 14:00:09 -0700

    add sprock task

--- macros/livetable/tasks/schedule_sp_fact_transactions.sql
+++ macros/livetable/tasks/schedule_sp_fact_transactions.sql
@@ -2,11 +2,15 @@
 CREATE OR REPLACE TASK near_dev.live_table.task_update_recent_hybrid_tx
 WAREHOUSE = DBT_CLOUD 
 SCHEDULE = '5 MINUTE' 
-ALLOW_OVERLAPPING_EXECUTION = TRUE 
+ALLOW_OVERLAPPING_EXECUTION = FALSE 
 AS
+    DECLARE
+        sp_return_value STRING;
     BEGIN
         ALTER SESSION SET QUERY_TAG = 'sp_near_live_fact_transactions';
-        CALL sp_update_recent_hybrid_transactions();
+        CALL sp_update_recent_hybrid_transactions() INTO :sp_return_value;
+
+        SYSTEM$SET_RETURN_VALUE(:sp_return_value);
     END;
 {% endmacro %}
 
