d00cd22ec1e3bc46373045717c1f46ee843591e3
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-04-28 09:31:20 -0600

    upd gold and complete nft sales

--- models/gold/nft/nft__ez_nft_sales.sql
+++ models/gold/nft/nft__ez_nft_sales.sql
@@ -5,7 +5,6 @@
 ) }}
 
 WITH nft_sales AS (
-
     SELECT
         receipt_id,
         block_id,
@@ -29,8 +28,8 @@ WITH nft_sales AS (
         platform_fee,
         platform_fee_usd,
         nft_sales_id AS ez_nft_sales_id,
-        COALESCE(inserted_timestamp, _inserted_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) AS inserted_timestamp,
-        COALESCE(modified_timestamp, _inserted_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) AS modified_timestamp
+        inserted_timestamp,
+        modified_timestamp
     FROM
         {{ ref('silver__nft_complete_nft_sales') }}
 )
