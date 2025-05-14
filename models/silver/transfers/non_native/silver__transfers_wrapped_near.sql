a6d6a5d4f38898d0400b41b46920a83d1989cfd0
jacksan <jmfxyz@pm.me>
2025-05-14 16:06:08 -0600

    wnear transfers init

---
+++ models/silver/transfers/non_native/silver__transfers_wrapped_near.sql
@@ -0,0 +1,76 @@
+{{ config(
+    materialized = 'incremental',
+    merge_exclude_columns = ["inserted_timestamp"],
+    incremental_predicates = ["dynamic_range_predicate_custom","block_timestamp::date"],
+    cluster_by = ['block_timestamp::DATE', 'modified_timestamp::DATE'],
+    unique_key = 'transfers_wrapped_near_id',
+    incremental_strategy = 'merge',
+    tags = ['scheduled_non_core']
+) }}
+
+WITH actions AS (
+
+    SELECT
+        block_id,
+        block_timestamp,
+        tx_hash,
+        receipt_id,
+        receipt_receiver_id AS contract_address,
+        receipt_receiver_id AS from_address,
+        receipt_predecessor_id AS to_address,
+        action_data :args :amount :: STRING AS amount_unadj,
+        NULL AS memo,
+        action_index AS rn,
+        FLOOR(
+            block_id,
+            -3
+        ) AS _partition_by_block_number
+    FROM
+        {{ ref('core__ez_actions') }}
+    WHERE
+        action_name = 'FunctionCall'
+        AND receipt_receiver_id = 'wrap.near'
+        AND receipt_succeeded
+        AND action_data :method_name :: STRING IN (
+            'near_deposit',
+            'near_withdraw'
+        ) 
+        -- temp lookback for dev
+        AND block_timestamp :: DATE > sysdate() - interval '90 days'
+    {% if var("MANUAL_FIX") %}
+        AND {{ partition_load_manual(
+            'no_buffer',
+            'floor(block_id, -3)'
+        ) }}
+    {% else %}
+
+        {% if is_incremental() %}
+        AND modified_timestamp >= (
+            SELECT
+                MAX(modified_timestamp)
+            FROM
+                {{ this }}
+        )
+        {% endif %}
+    {% endif %}
+)
+SELECT
+    block_id,
+    block_timestamp,
+    tx_hash,
+    receipt_id,
+    contract_address,
+    from_address,
+    to_address,
+    amount_unadj,
+    memo,
+    rn,
+    _partition_by_block_number,
+    {{ dbt_utils.generate_surrogate_key(
+        ['receipt_id', 'amount_unadj', 'from_address', 'to_address', 'rn']
+    ) }} AS transfers_wrapped_near_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    actions
